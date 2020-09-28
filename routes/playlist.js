95562b86682c854445587c50ce1d5b39d6c743d3
mohahakram <you@example.com>
2020-09-28 19:23:31 +0200

    used replace method to replace '-' by space

--- routes/playlist.js
+++ routes/playlist.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
-const beatModel = require('../models/Beats');
+const Beat = require('../models/Beats');
 
 //* get all songs from specific type
 router.get('/:id', function(req, res, next) {
-    console.log(req.params.id)
-    beatModel
-        .find( { type: { $eq: req.params.id } } )
+    // replace any '-' to space as types in db dont have '-' character
+    const type = req.params.id.replace('-',' ')
+    Beat
+        .find( { type: { $eq: type } } )
         .then( dbRes => {
             res.status(200).json(dbRes)
             console.log(dbRes)
@@ -16,7 +17,7 @@ router.get('/:id', function(req, res, next) {
 
 //* get all songs from specific artist
 router.get('/artist/:id', (req, res) => {
-    beatModel
+    Beat
         .find( { artist: { $eq: req.params.id } } )
         .then( dbRes => {
             res.status(200).json(dbRes)
