6b62a73a0cd2e9b3eefd547ad395409b523b17fa
mohahakram <you@example.com>
2020-09-28 19:18:46 +0200

    added jwt strategy

--- config/passport.js
+++ config/passport.js
@@ -1,71 +1,29 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
 const passport = require('passport');
-const LocalStrategy = require('passport-local').Strategy;
 const JwtStrategy = require("passport-jwt").Strategy;
-const ExtractJwt = require("passport-jwt").ExtractJwt;
-const User = require('../models/User')
+const User = require('../models/Users');
 
-// passport.use( new LocalStrategy({
-//     usernameField: 'email'
-// }, async (email, password, done) => {
-    
-//     User.findOne( { email: email } )
-//         .then( user => {
-//             if (err) {
-//                 return done(err, null);
-//             }
-//             if (!user) {
-//                 return done(null, false, "Incorrect infos" ) //no error, user not found, message
-//             }
-//             if (!bcrypt.compare(password, user.password) ) {
-//                 return done(null, false, "Incorrect password")
-//             } else {
-//                 return done(null, user)//no error, return user
-//             }
-//         })
-//         .catch( (err) => {
-//             done(err);
-//         })
-// }));
+// extract cookie from cookie in request header
+const cookieExtractor = req => {
+  let token = null;
+  if (req && req.cookies) {
+    token = req.cookies['auth_token'];
+  }
+  return token;
+};
 
-// const verifyCallback = (email, password, done) => {
+// options to pass into strategy
+const options = {
+  jwtFromRequest: cookieExtractor,
+  secretOrKey: process.env.JWT_SECRET,
+  algorithms: ['HS256']
 
-//     userModel.findOne( { email: email } )
-//         .then( user => {
-//             if (err){
-//                 return done(err, null);
-//             }
-//             if ( !user ) {
-//                 return done(null, false, "Incorrect infos" ) //no error, user not found, message
-//             }
-//             if ( !bcrypt.compare(password, user.password) ){
-//                 return done(null, false, "Incorrect password")
-//             } else {
-//                 return done(null, user)//no error, return user
-//             }
-//         })
-//         .catch( (err) => {
-//             done(err);
-//         })
-// }
+};
 
-// const localStrategy = new LocalStrategy( { usernameField: "email"}, verifyCallback)
-
-// passport.use(localStrategy);
-
-
-const opts = {};
-
-opts.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
-opts.secretOrKey = process.env.JWT_SECRET;
-
-
-
-module.exports = passport => {
-  passport.use(
-    new JwtStrategy(opts, (jwt_payload, done) => {
-      User.findById(jwt_payload.id)
+// deconstruct cookie then search db with id then return matching user
+const strategy = new JwtStrategy(options, (payload, done) => {
+  User.findById(payload.id)
         .then(user => {
           if (user) {
             return done(null, user);
@@ -73,6 +31,8 @@ module.exports = passport => {
           return done(null, false);
         })
         .catch( err => done(console.error(err)));
-    })
-  );
-};
+});
+
+module.exports = (passport) => {
+  passport.use(strategy);
+}
