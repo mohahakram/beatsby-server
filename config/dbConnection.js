8bc5e33e832f1d4dc0b2a23d77d3989fbf8b1116
mohahakram <you@example.com>
2020-09-28 16:56:00 +0200

    check for environment to set db URI

--- config/dbConnection.js
+++ config/dbConnection.js
@@ -1,15 +1,18 @@
 require('dotenv').config();
 const mongoose = require('mongoose')
 
+const uri = process.env.NODE_ENV === 'development' ? process.env.DB_URI : process.env.DB_ATLAS_URI;
+
 const connection = mongoose
-    .connect(process.env.DB_URI,    {useNewUrlParser: true,
-                                    useCreateIndex: true,
-                                    useUnifiedTopology: true})
-    .then(dbConnectionResult => {
+    .connect( uri,    
+        {useNewUrlParser: true,
+        useCreateIndex: true,
+        useUnifiedTopology: true})
+    .then( dbConnectionResult => {
         console.log(`Connected to Mongo! Database name: ${dbConnectionResult.connections[0].name}`)
     })
     .catch( err => {
         console.log('Error connecting to Mongo', err);
     });
 
-    module.exports = connection;
+module.exports = connection;
