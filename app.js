ba19fc958bce6fd1f7d4b80c161aa1af8c16fda7
mohahakram <you@example.com>
2020-09-29 15:34:53 +0200

    added port to deploy with heroku

--- app.js
+++ app.js
@@ -68,6 +68,7 @@ app.use('/checkout', passport.authenticate('jwt', {session: false}), require('./
 
 //*------------------SERVER---------------------
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server running on port: ${process.env.PORT}`)
+let port = process.env.PORT || 4001
+app.listen( port, () => {
+    console.log(`Server running on port: ${port}`)
 }) 
