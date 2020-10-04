db2499e5db512904426b862c10e84ece507b2f78
mohahakram <you@example.com>
2020-10-04 19:55:43 +0200

    changed method to get jwt from header

--- config/passport.js
+++ config/passport.js
@@ -1,21 +1,31 @@
 require('dotenv').config();
 const mongoose = require("mongoose");
 const passport = require('passport');
+const { ExtractJwt } = require('passport-jwt');
 const JwtStrategy = require("passport-jwt").Strategy;
 const User = require('../models/Users');
 
 // extract cookie from cookie in request header
-const cookieExtractor = req => {
-  let token = null;
-  if (req && req.cookies) {
-    token = req.cookies['auth_token'];
-  }
-  return token;
-};
+// const cookieExtractor = req => {
+//   console.log(req.headers);
+//   let token = null;
+//   if (req && req.cookies) {
+//     token = req.cookies['auth_token'];
+//   }
+//   return token;
+// };
 
-// options to pass into strategy
+// options to pass into strategy //cookie from cookies
+// const options = {
+//   jwtFromRequest: cookieExtractor,
+//   secretOrKey: process.env.JWT_SECRET,
+//   algorithms: ['HS256']
+
+// };
+
+// options to pass into strategy, cookie from header
 const options = {
-  jwtFromRequest: cookieExtractor,
+  jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
   secretOrKey: process.env.JWT_SECRET,
   algorithms: ['HS256']
 
